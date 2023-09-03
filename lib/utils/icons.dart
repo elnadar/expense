fcde4a1688a5b4a7c92aa8ab65ad394774539a3a
Ahmed Mahdy <mo.a9tay@gmail.com>
2023-09-03 13:11:51 +0300

    solved some issues

---
+++ lib/utils/icons.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+abstract final class IconsMap {
+  static const Map<String, IconData> icons = {
+    "food_bank": Icons.food_bank,
+    "coffee": Icons.coffee,
+    "games": Icons.games,
+  };
+}
